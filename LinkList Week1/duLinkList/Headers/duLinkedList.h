22488816728dfe91dfbacb37efba31de2ed08a21
Flychee <101914034+Flychee@users.noreply.github.com>
2022-03-21 23:28:23 +0800

    Add files via upload

---
+++ LinkList Week1/duLinkList/Headers/duLinkedList.h
@@ -0,0 +1,194 @@
+/***************************************************************************************
+ *	File Name				:	duLinkedList.h
+ *	CopyRight				:	2020 QG Studio
+ *	SYSTEM					:   win10
+ *	Create Data				:	2020.3.28
+ *
+ *
+ *--------------------------------Revision History--------------------------------------
+ *	No	version		Data			Revised By			Item			Description
+ *
+ *
+ ***************************************************************************************/
+
+/**************************************************************
+*	Multi-Include-Prevent Section
+**************************************************************/
+
+#ifndef DULINKEDLIST_H_INCLUDED
+#define DULINKEDLIST_H_INCLUDED
+
+/**************************************************************
+*	Macro Define Section
+**************************************************************/
+
+#define OVERFLOW -1
+
+/**************************************************************
+*	Struct Define Section
+**************************************************************/
+
+// define element type
+typedef int ElemType;
+
+// define struct of linked list
+typedef struct DuLNode {
+	ElemType data;
+  	struct DuLNode *prior,  *next;
+} DuLNode, *DuLinkedList;
+
+// define status
+typedef enum Status {
+	ERROR,
+	SUCCESS,
+} Status;
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**************************************************************
+*	Prototype Declare Section
+**************************************************************/
+
+/**
+ *  @name        : Status InitList_DuL(DuLinkedList *L)
+ *	@description : initialize an empty linked list with only the head node
+ *	@param		 : L(the head node)
+ *	@return		 : Status
+ *  @notice      : None
+ */
+Status InitList_DuL(DuLinkedList *L)
+{
+	int num = 0;
+	DuLNode *head = NULL;
+	DuLNode *q = NULL;
+	DuLNode *p = NULL;
+	printf("请输入节点数:  ");
+	scanf("%d", &num);
+	for(int i = 1; i <=num ; i++)
+	{
+		p=(DuLNode*)malloc(sizeof(DuLNode));
+		if(i != 1)
+		{
+			q->next = p;
+			p->prior = q;
+		}
+		else
+		{
+			head = q = p;
+			p->prior = NULL;
+		}
+		p->next = NULL;
+		printf("请输入第%d个节点的数据:  ", i);
+		scanf("%d",&(p->data));
+		q = p;
+		p = p->next;
+	}
+	*L = head;
+	if(L != NULL) return SUCCESS;
+	else return ERROR;
+};
+
+/**
+ *  @name        : void DestroyList_DuL(DuLinkedList *L)
+ *	@description : destroy a linked list
+ *	@param		 : L(the head node)
+ *	@return		 : status
+ *  @notice      : None
+ */
+void DestroyList_DuL(DuLinkedList *L)
+{
+	if(*L == NULL) return ;
+	DuLNode *p = NULL;/*遍历链表用*/
+	DuLNode *q = NULL;
+	p = *L;
+	while(p != NULL)
+	{
+		q = p;
+		p = p->next;
+		free(q);
+	}
+};
+
+/**
+ *  @name        : Status InsertBeforeList_DuL(DuLNode *p, LNode *q)
+ *	@description : insert node q before node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertBeforeList_DuL(DuLNode *p, DuLNode *q)
+{
+	if(p == NULL || q == NULL) return ERROR;
+	q->prior = p->prior;
+	q->next = p;
+	if(p->prior != NULL)
+	p->prior->next = q;
+	p->prior = q;
+	return SUCCESS;
+};
+
+/**
+ *  @name        : Status InsertAfterList_DuL(DuLNode *p, DuLNode *q)
+ *	@description : insert node q after node p
+ *	@param		 : p, q
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status InsertAfterList_DuL(DuLNode *p, DuLNode *q)
+{
+	if(p == NULL || q == NULL) return ERROR;
+	q->prior = p;
+	q->next = p->next;
+	if(p->next != NULL)
+	p->next->prior = q;
+	p->next = q;
+	return SUCCESS;
+};
+/**
+ *  @name        : Status DeleteList_DuL(DuLNode *p, ElemType *e)
+ *	@description : delete the first node after the node p and assign its value to e
+ *	@param		 : p, e
+ *	@return		 : status
+ *  @notice      : None
+ */
+Status DeleteList_DuL(DuLNode *p, ElemType *e)
+{
+	if(p == NULL) return ERROR;
+	DuLNode *temp = NULL;
+	temp = p->next;
+	p->next = p->next->next;
+	*e = temp->data;
+	free(temp);
+	return SUCCESS;
+};
+
+/**
+ *  @name        : void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e))
+ *	@description : traverse the linked list and call the funtion visit
+ *	@param		 : L(the head node), visit
+ *	@return		 : Status
+ *  @notice      : None
+ */
+void TraverseList_DuL(DuLinkedList L, void (*visit)(ElemType e))
+{
+	DuLNode *p = L;
+	while (p != NULL)
+	{
+		visit(p->data);
+		p = p->next;
+	}
+	printf("NULL\n");/*链表遍历结束标志*/
+};
+
+void visit(ElemType e)/*数据输出*/
+{
+	printf("%d -> ",e);
+};
+
+
+/**************************************************************
+*	End-Multi-Include-Prevent Section
+**************************************************************/
+#endif
---
+++ LinkList Week1/duLinkList/Release/main.c
@@ -0,0 +1,120 @@
+#include "duLinkedList.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void screen()
+{
+    printf("1.创建链表\n");
+    printf("2.销毁链表\n");
+    printf("3.前驱插入节点\n");
+    printf("4.后驱插入节点\n");
+    printf("5.删除结点\n");
+    printf("6.遍历链表\n");
+    printf("7.退出程序\n");
+    printf("\n请输入1-7: \n");
+}
+
+void end()
+{
+    printf("选项已执行\n");
+    system("pause");
+    system("cls");
+}
+
+int main()
+{
+    int num = 0;
+    int scf;
+    int result;
+    char ch;
+    ElemType my_data = OVERFLOW;
+    DuLinkedList my_list;
+    DuLNode *head = NULL;
+    DuLNode *now = NULL;
+    DuLNode *insert = NULL;
+    for(;;)
+    {
+        screen();
+        scanf("%d",&num);
+        ch = getchar();//清除缓冲区
+        system("cls");
+        if(num < 1 || num > 7) continue;
+        else
+        {
+            switch(num)
+            {
+                case 7: exit(0);
+
+                case 6:
+                    TraverseList_DuL(my_list,visit);
+                    break;
+                case 5:
+                    printf("请输入删除结点序号(最小为2)： ");
+                    scanf("%d",&scf);
+                    ch = getchar();//清除缓冲区
+                    if(scf<2) printf("输入错误");
+                    else
+                    {
+                        now = my_list;
+                        for(int i=1;i<scf;i++)
+                        {
+                            if(now == NULL)
+                            break;
+                            else now = now->next;
+                        }
+                        DeleteList_DuL(now,&my_data);
+                        now = NULL;
+                        my_data = OVERFLOW;
+                        break;
+                    }
+                case 4:
+                    printf("请输入需插入的结点序号(最小为1)： ");
+                    scanf("%d",&scf);
+                    ch = getchar();//清除缓冲区
+                    now = my_list;
+                    for(int i=1;i<scf;i++)
+                    {
+                        if(now == NULL)
+                        break;
+                        else now = now->next;
+                        
+                    }
+                    insert = (DuLNode*)malloc(sizeof(DuLNode));
+                    insert->next =NULL;
+                    printf("请输入需插入的数据(值尽量不为-1)： ");
+                    scanf("%d",&(insert->data));
+                    ch = getchar();//清除缓冲区
+                    InsertAfterList_DuL(now,insert);
+                    break;
+                case 3:
+                    printf("请输入需插入的结点序号(最小为1)： ");
+                    scanf("%d",&scf);
+                    ch = getchar();//清除缓冲区
+                    now = my_list;
+                    for(int i=1;i<scf;i++)
+                    {
+                        if(now == NULL)
+                        break;
+                        else now = now->next;
+                        
+                    }
+                    insert = (DuLNode*)malloc(sizeof(DuLNode));
+                    insert->prior = NULL;
+                    insert->next = NULL;
+                    printf("请输入需插入的数据(值尽量不为-1)： ");
+                    scanf("%d",&(insert->data));
+                    ch = getchar();//清除缓冲区
+                    InsertBeforeList_DuL(now,insert);
+                    if(now == my_list) my_list = my_list->prior;/*在头结点前驱插入后使头结点前移*/
+                    break;
+                case 2:
+                    DestroyList_DuL(&my_list);
+                    break;
+                case 1:
+                    InitList_DuL(&my_list);
+                    break;
+            }
+        end();
+        }
+    }
+}
